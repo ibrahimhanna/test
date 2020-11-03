a44ddbb86fc82cd91f0424906dd6b4fbe45932c1
Ibrahim Hanna <ibrahim.seniore@gmail.com>
2020-11-03 12:24:25 +0200

    welcome to updated file

--- TestProject/src/com/tester/Read.java
+++ TestProject/src/com/tester/Read.java
@@ -2,9 +2,4 @@ package com.tester;
 
 public class Read {
 
-	public static void main(String[] args){
-		
-	}
-	
-	
 }
--- TestProject/src/com/tester/Test.java
+++ TestProject/src/com/tester/Test.java
@@ -2,11 +2,17 @@ package com.tester;
 
 public class Test {
 
-	public static void main(String[] args) {
+	public static void main(String [] args) {
+		
+		int sum = 10 ;
+		System.out.println(sum);
 		
 		
-		int sum = 5+5;
-		System.out.print(sum);
+		for(int i = 0; i < 15; i++) {
+			sum ++;
+			if(sum == 15)break;
+			System.out.println(sum);
+		}
 		
 		
 	}
